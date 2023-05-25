b10d39cf0ef8f044c9bd52bf3fa8bc8be5d57b6a
lifufkd <79800838+lifufkd@users.noreply.github.com>
2023-05-25 21:28:46 +1000

    Add files via upload

---
+++ send.php
@@ -0,0 +1,10 @@
+<?php
+// Сообщение
+$message = "Line 1\r\nLine 2\r\nLine 3";
+
+// На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Отправляем
+mail('ad757041@gmail.com', 'My Subject', $message);
+?>
